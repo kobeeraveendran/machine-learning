2eddfc90a1b524b6fc463e0a4cb7b28add7e75c0
Kobee Raveendran <32165720+kobeeraveendran@users.noreply.github.com>
2018-03-06 21:22:27 -0500

    added testing functions for prediction
    commented-out code blocks can be commented in to go back to substitute in a real dataset for implementation, as well as the data plot

--- learning/k_nearest_neighbors_custom.py
+++ learning/k_nearest_neighbors_custom.py
@@ -8,6 +8,8 @@ import warnings
 from matplotlib import style
 from math import sqrt
 from collections import Counter
+import pandas as pd 
+import random
 
 style.use('fivethirtyeight')
 
@@ -17,13 +19,13 @@ plot1 = [1,3]
 plot2 = [2,5]
 euclidean_distance = sqrt((plot1[0] - plot2[0]) ** 2 + (plot1[1] - plot2[1]) ** 2)
 print(euclidean_distance)
-'''
+
 
 # sample dataset with two classes (features k and r)
 dataset = {'k': [[1,2], [2,3], [3,1]], 'r':[[6,5], [7,7], [8,6]]}
 
 new_features = [5,7]
-
+'''
 
 
 # k-nearest neighbors function, with default k-value of 3
@@ -40,11 +42,46 @@ def k_nearest_neighbors(data, predict, k = 3):
             distances.append([euclidean_distance, group])
 
     votes = [i[1] for i in sorted(distances)[:k]]
-    print(Counter(votes).most_common)
     vote_result = Counter(votes).most_common(1)[0][0]
      
     return vote_result
 
+df = pd.read_csv('breast-cancer-wisconsin.data.txt')
+df.replace('?', -99999, inplace = True)
+df.drop(['id'], 1, inplace = True)
+
+full_data = df.astype(float).values.tolist()
+
+random.shuffle(full_data)
+
+test_size = 0.2
+train_set = {2:[], 4:[]}
+test_set = {2:[], 4:[]}
+train_data = full_data[:-int(test_size * len(full_data))]
+test_data = full_data[-int(test_size * len(full_data)):]
+
+for i in train_data:
+    train_set[i[-1]].append(i[:-1])
+
+for i in test_data:
+    test_set[i[-1]].append(i[:-1])
+
+correct = 0
+total = 0
+
+for group in test_set:
+    for data in test_set[group]:
+        vote = k_nearest_neighbors(train_set, data, k = 5)
+        if group == vote:
+            correct += 1
+        total += 1
+
+print("correct:", correct)
+print("total", total)
+print("Accuracy:", correct / total)
+
+# not included in testing phase
+'''
 result = k_nearest_neighbors(dataset, new_features, k = 3)
 print(result)
 
@@ -54,3 +91,4 @@ for i in dataset:
 
 plt.scatter(new_features[0], new_features[1], color = result, s = 100)
 plt.show()
+'''
