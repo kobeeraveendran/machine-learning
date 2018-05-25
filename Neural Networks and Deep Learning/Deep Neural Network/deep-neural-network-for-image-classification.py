29e3f64b48060dd0e329dd21190ed247f077a6d9
Kobee Raveendran <32165720+kobeeraveendran@users.noreply.github.com>
2018-05-25 16:34:52 -0400

    added initialize_parameters() method

--- Neural Networks and Deep Learning/Deep Neural Network/deep-neural-network-for-image-classification.py
+++ Neural Networks and Deep Learning/Deep Neural Network/deep-neural-network-for-image-classification.py
@@ -56,7 +56,19 @@ print("test_x's shape: " + str(test_x.shape))
 
 # 2 layer neural net
 
+def initialize_parameters(n_x, n_h, n_y):
+    np.random.seed(1)
 
+    W1 = np.random.randn(n_h, n_x) * 0.01
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.randn(n_y, n_h) * 0.01
+    b2 = np.zeros((n_y, 1))
+
+    parameters = {'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
+
+    return parameters
+
+# L-layer neural network
 
 def initialize_parameters_deep(layer_dims):
     np.random.seed(3)
