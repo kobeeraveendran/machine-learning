c97d514d0ec03d1fb3ee8a4e5b38535f452e101a
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-07-13 23:40:33 -0400

    added optimizer, loss function, and accuracy metric

--- deep-learning-tensorflow/basic_classification.py
+++ deep-learning-tensorflow/basic_classification.py
@@ -54,3 +54,6 @@ model = keras.Sequential([keras.layers.Flatten(input_shape = (28, 28)),
                           keras.layers.Dense(128, activation = tf.nn.relu), 
                           keras.layers.Dense(10, activation = tf.nn.softmax)])
 
+model.compile(optimizer = tf.train.AdamOptimizer(), 
+              loss = 'sparse_categorical_crossentropy', 
+              metrics = ['accuracy'])
