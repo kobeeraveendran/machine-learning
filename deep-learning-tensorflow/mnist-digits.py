c32528ed726231b4021fb8f3393342e905dbc01b
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-07-15 17:36:04 -0400

    reshaped and flattened train/test sets, defined 2-layer model with optimizer and loss function

--- deep-learning-tensorflow/mnist-digits.py
+++ deep-learning-tensorflow/mnist-digits.py
@@ -1,6 +1,7 @@
 import tensorflow as tf 
 from tensorflow import keras
 import matplotlib.pyplot as plt
+import numpy as np
 
 mnist = keras.datasets.mnist
 (train_data, train_labels), (test_data, test_labels) = mnist.load_data()
@@ -14,3 +15,18 @@ print("Testing label shape: " + str(test_labels.shape))
 plt.figure()
 plt.imshow(train_data[0])
 plt.show()
+
+# reshape and flatten for model compatibility
+train_data = np.reshape(train_data, (60000, 784))
+test_data = np.reshape(test_data, (10000, 784))
+
+# restrict pixel values
+train_data = train_data.astype('float32') / 255.0
+test_data = train_data.astype('float32') / 255.0
+
+# define model
+model = keras.models.Sequential()
+model.add(keras.layers.Dense(512, activation = 'relu', input_shape = (784, )))
+model.add(keras.layers.Dense(10, activation = 'softmax'))
+
+model.compile(optimizer = 'rmsprop', loss = 'categorical_crossentropy', metrics = ['accuracy'])
