d31a391b643b165ed9698fb6f381bc19604b5d61
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-07-16 01:26:21 -0400

    added predictions for a sample image and displayed image for verification

--- deep-learning-tensorflow/mnist-digits.py
+++ deep-learning-tensorflow/mnist-digits.py
@@ -16,7 +16,7 @@ print("Testing label shape: " + str(test_labels.shape))
 
 # restrict pixel values
 train_data = train_data.astype('float32') / 255
-test_data = train_data.astype('float32') / 255
+test_data = test_data.astype('float32') / 255
 
 # categorize labels
 train_labels = keras.utils.to_categorical(train_labels)
@@ -32,3 +32,16 @@ model = models.Sequential([
 model.compile(optimizer = 'rmsprop', loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
 model.fit(x = train_data, y = train_labels, epochs = 5, batch_size = 128)
+
+# evaluate model on test set
+test_loss, test_acc = model.evaluate(test_data, test_labels)
+print("Test accuracy: " + str(test_acc * 100) + "%")
+
+# make and visualize prediction
+predictions = model.predict(test_data)
+print("\nPrediction for image 4: " + str(np.argmax(predictions[3])))
+print("Actual value for image 4: " + str(np.argmax(test_labels[3])))
+
+plt.figure()
+plt.imshow(test_data[3])
+plt.show()
