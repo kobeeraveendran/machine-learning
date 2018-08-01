dcd2604bf39f71ab659571b858751a689f9b1496
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-01 14:22:58 -0400

    added forward propagation with dropout

--- Hyperparameter tuning, Regularization and Optimization/Practical Aspects of Deep Learning/Regularization.py
+++ Hyperparameter tuning, Regularization and Optimization/Practical Aspects of Deep Learning/Regularization.py
@@ -95,6 +95,42 @@ def backward_propagation_with_regularization(X, Y, cache, lambd):
     return gradients
 
 
+def forward_propagation_with_dropout(X, parameters, keep_prob = 0.5):
+
+    np.random.seed(1)
+
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    W3 = parameters["W3"]
+    b3 = parameters["b3"]
+
+    Z1 = np.dot(W1, X) + b1
+    A1 = relu(Z1)
+
+    D1 = np.random.rand(A1.shape[0], A1.shape[1])
+    D1 = (D1 < keep_prob)
+    A1 = np.multiply(D1, A1)
+    A1 = A1 / keep_prob
+
+    Z2 = np.dot(W2, A1) + b2
+    A2 = relu(Z2)
+
+    D2 = np.random.rand(A2.shape[0], A2.shape[1])
+    D2 = (D2 < keep_prob)
+    A2 = np.multiply(D2, A2)
+    A2 = A2 / keep_prob
+
+    Z3 = np.dot(W3, A2) + b3
+    A3 = sigmoid(Z3)
+
+    cache = (Z1, D1, A1, W1, b1, Z2, D2, A2, W2, b2, Z3, A3, W3, b3)
+
+    return A3, cache
+
+
+
 parameters = model(train_X, train_Y)
 print("Training set: ")
 predictions_train = predict(train_X, train_Y, parameters)
@@ -135,3 +171,13 @@ axes = plt.gca()
 axes.set_xlim([-0.75, 0.40])
 axes.set_ylim([-0.75, 0.40])
 plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+
+################
+# WITH DROPOUT #
+################
+
+# forward propagation
+X_assess, parameters = forward_propagation_with_dropout_test_case()
+
+A3, cache = forward_propagation_with_dropout(X_assess, parameters, keep_prob = 0.7)
+print("A3 = " + str(A3))
