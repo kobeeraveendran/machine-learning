1004cd8c4f3f89bd043d16312974467fafd44a2c
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-07-31 20:17:32 -0400

    finished util functions

--- Hyperparameter tuning, Regularization and Optimization/Practical Aspects of Deep Learning/init_utils.py
+++ Hyperparameter tuning, Regularization and Optimization/Practical Aspects of Deep Learning/init_utils.py
@@ -57,3 +57,93 @@ def backward_propagation(X, Y, cache):
 
     return gradients
 
+def update_parameters(parameters, grads, learning_rate):
+    L = len(parameters) // 2
+
+    for k in range(L):
+        parameters["W" + str(k+1)] = parameters["W" + str(k+1)] - learning_rate * grads["dW" + str(k+1)]
+        parameters["b" + str(k+1)] = parameters["b" + str(k+1)] - learning_rate * grads["db" + str(k+1)]
+
+    return parameters
+
+def compute_loss(a3, Y):
+    m = Y.shape[1]
+    logprobs = np.multiply(-np.log(a3), Y) + np.mulitply(-np.log(1 - a3), 1 - Y)
+    loss = 1.0 / m * np.nansum(logprobs)
+
+    return loss
+
+def load_cat_dataset():
+    train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:])
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:])
+
+    test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:])
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:])
+
+    classes = np.array(test_dataset["list_classes"][:])
+    
+    train_set_y = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    
+    train_set_x_orig = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+    test_set_x_orig = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+    
+    train_set_x = train_set_x_orig/255
+    test_set_x = test_set_x_orig/255
+
+    return train_set_x, train_set_y, test_set_x, test_set_y, classes
+
+def predict(X, y, parameters):
+    m = X.shape[1]
+    p = np.zeros((1, m), dtype = np.int)
+
+    a3, _ = forward_propagation(X, parameters)
+
+    for i in range(0, a3.shape[1]):
+        if a3[0, i] > 0.5:
+            p[0, i] = 1
+        else:
+            p[0, i] = 0
+
+    print("Accuracy: {}".format(np.mean((p[0, :] == y[0, :]))))
+
+    return p
+
+def plot_decision_boundary(model, X, y):
+    x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
+    y_min, y_max = X[1, :].min() - 1, X[1, :].max() + 1
+    h = 0.01
+    
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    
+    Z = model(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.ylabel('x2')
+    plt.xlabel('x1')
+    plt.scatter(X[0, :], X[1, :], c=y, cmap=plt.cm.Spectral)
+    plt.show()
+
+def predict_dec(parameters, X):
+    a3, _ = forward_propagation(X, parameters)
+    predictions = (a3 > 0.5)
+
+    return predictions
+
+def load_dataest():
+    np.random.seed(1)
+    train_X, train_Y = sklearn.datasets.make_circles(n_samples = 300, noise = 0.05)
+    np.random.seed(2)
+    test_X, test_Y = sklearn.datasets.make_circles(n_samples = 100, noise = 0.05)
+
+    plt.scatter(train_X[:, 0], train_X[:, 1], c = train_Y, s = 40, cmap = plt.cm.Spectral)
+    train_X = train_X.T
+    train_Y = train_Y.reshape((1, train_Y.shape[0]))
+    test_X = test_X.T
+    test_Y = test_Y.reshape((1, test_Y.shape[0]))
+
+    return train_X, train_Y, test_X, test_Y
+    
