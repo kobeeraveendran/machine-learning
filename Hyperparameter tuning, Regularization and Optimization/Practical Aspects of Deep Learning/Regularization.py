d7ff16c9b3b772a6329052557fa247ef63879e4f
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-01 14:36:49 -0400

    added backpropagation with dropout

--- Hyperparameter tuning, Regularization and Optimization/Practical Aspects of Deep Learning/Regularization.py
+++ Hyperparameter tuning, Regularization and Optimization/Practical Aspects of Deep Learning/Regularization.py
@@ -129,7 +129,36 @@ def forward_propagation_with_dropout(X, parameters, keep_prob = 0.5):
 
     return A3, cache
 
+def backward_propagation_with_dropout(X, Y, cache, keep_prob):
 
+    m = X.shape[1]
+    (Z1, D1, A1, W1, b1, Z2, D2, A2, W2, b2, Z3, A3, W3, b3) = cache
+
+    dZ3 = A3 - Y
+    dW3 = (1. / m) * np.dot(dZ3, A2.T)
+    db3 = (1. / m) * np.sum(dZ3, axis = 1, keepdims = True)
+    dA2 = np.dot(W3.T, dZ3)
+
+    dA2 = np.multiply(D2, dA2)
+    dA2 = dA2 / keep_prob
+
+    dZ2 = np.multiply(dA2, np.int64(A2 > 0))
+    dW2 = (1. / m) * np.dot(dZ2, A1.T)
+    db2 = (1. / m) * np.sum(dZ2, axis = 1, keepdims = True)
+
+    dA1 = np.dot(W2.T, dZ2)
+    dA1 = np.multiply(D1, dA1)
+    dA1 = dA1 / keep_prob
+
+    dZ1 = np.multiply(dA1, np.int64(A1 > 0))
+    dW1 = (1. / m) * np.dot(dZ1, X.T)
+    db1 = (1. / m) * np.sum(dZ1, axis = 1, keepdims = True)
+
+    gradients = {"dZ3": dZ3, "dW3": dW3, "db3": db3,"dA2": dA2,
+                 "dZ2": dZ2, "dW2": dW2, "db2": db2, "dA1": dA1, 
+                 "dZ1": dZ1, "dW1": dW1, "db1": db1}
+
+    return gradients
 
 parameters = model(train_X, train_Y)
 print("Training set: ")
@@ -181,3 +210,12 @@ X_assess, parameters = forward_propagation_with_dropout_test_case()
 
 A3, cache = forward_propagation_with_dropout(X_assess, parameters, keep_prob = 0.7)
 print("A3 = " + str(A3))
+
+# backpropagation
+parameters = model(train_X, train_Y, keep_prob = 0.86, learning_rate = 0.3)
+
+print("Training set: ")
+predictions_train = predict(train_X, train_Y, parameters)
+print("Testing set: ")
+predictions_test = predict(test_X, test_Y, parameters)
+
