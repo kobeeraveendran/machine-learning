a17330b5825111e468db907ecfd64ac289ebbe0b
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-01 14:39:40 -0400

    fixed minor compile time issue with imports

--- Hyperparameter tuning, Regularization and Optimization/Practical Aspects of Deep Learning/Regularization.py
+++ Hyperparameter tuning, Regularization and Optimization/Practical Aspects of Deep Learning/Regularization.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from reg_utils import sigmoid, relu, plot_decision_boundary, initialize_parameters, load_2D_dataset, predict_dec
 from reg_utils import compute_cost, predict, forward_propagation, backward_propagation, update_parameters
-import sklean
+import sklearn
 import sklearn.datasets
 import scipy.io
 from testCases import *
