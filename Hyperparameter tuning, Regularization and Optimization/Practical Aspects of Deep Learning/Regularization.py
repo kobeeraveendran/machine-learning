9b537aa98f6f7c13721d0426bc07018117a488b7
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-01 13:18:57 -0400

    added model function

--- Hyperparameter tuning, Regularization and Optimization/Practical Aspects of Deep Learning/Regularization.py
+++ Hyperparameter tuning, Regularization and Optimization/Practical Aspects of Deep Learning/Regularization.py
@@ -1,2 +1,56 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from reg_utils import sigmoid, relu, plot_decision_boundary, initialize_parameters, load_2D_dataset, predict_dec
+from reg_utils import compute_cost, predict, forward_propagation, backward_propagation, update_parameters
+import sklean
+import sklearn.datasets
+import scipy.io
+from testCases import *
+
+plt.rcParams['figure.figsize'] = (7.0, 4.0)
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+train_X, train_Y, test_X, test_Y = load_2D_dataset()
+
+def model(X, Y, learning_rate = 0.3, num_iterations = 30000, print_cost = True, lambd = 0, keep_prob = 1):
+    grads = {}
+    costs = []
+    m = X.shape[1]
+    layers_dims = [X.shape[0], 20, 3, 1]
+
+    parameters = initialize_parameters(layers_dims)
+
+    for i in range(num_iterations):
+        if keep_prob == 1:
+            a3, cache = forward_propagation(X, parameters)
+        
+        elif keep_prob < 1:
+            a3, cache = forward_propagation_with_dropout(X, parameters, keep_prob)
+
+        if lambd == 0:
+            cost = compute_cost(a3, Y)
+        else:
+            cost = compute_cost_with_regularization(a3, Y, parameters, lambd)
+
+        if lambd == 0 and keep_prob == 1:
+            grads = backward_propagation(X, Y, cache)
+        
+        elif lambd != 0:
+            grads = backward_propagation_with_regularization(X, Y, cache, lambd)
+
+        elif keep_prob < 1:
+            grads = backward_propagation_with_dropout(X, Y, cache, keep_prob)
+
+        if print_cost and i % 10000 == 0:
+            print('Cost after iteration {}: {}'.format(i, cost))
+            costs.append(cost)
+
+        plt.plot(costs)
+        plt.ylabel('cost')
+        plt.xlabel('iterations (in ten thousands)')
+        plt.title("Learning rate = " + str(learning_rate))
+        plt.show()
+
+        return parameters
+
