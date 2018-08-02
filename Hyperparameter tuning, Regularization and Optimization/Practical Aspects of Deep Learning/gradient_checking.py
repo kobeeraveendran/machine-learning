d73e9ea78fea730ebd34a933372e0fea905a6758
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-02 19:34:50 -0400

    added gradient checking for N-dimensional inputs, and corrected backprop function

--- Hyperparameter tuning, Regularization and Optimization/Practical Aspects of Deep Learning/gradient_checking.py
+++ Hyperparameter tuning, Regularization and Optimization/Practical Aspects of Deep Learning/gradient_checking.py
@@ -80,3 +80,57 @@ def forward_propagation_n(X, Y, parameters):
 
     return cost, cache
 
+def backward_propagation_n(X, Y, cache):
+
+    m = X.shape[1]
+    (Z1, A1, W1, b1, Z2, A2, W2, b2, Z3, A3, W3, b3) = cache
+
+    dZ3 = A3 - Y
+    dW3 = (1.0 / m) * np.dot(dZ3, A2.T)
+    db3 = (1.0 / m) * np.sum(dZ3, axis = 1, keepdims = True)
+    
+    dA2 = np.dot(W3.T, dZ3)
+    dZ2 = np.multiply(dA2, np.int64(A2 > 0))
+    dW2 = (1.0 / m) * np.dot(dZ2, A1.T)
+    db2 = (1.0 / m) * np.sum(dZ2, axis = 1, keepdims = True)
+    
+    dA1 = np.dot(W2.T, dZ2)
+    dZ1 = np.multiply(dA1, np.int64(A1 > 0))
+    dW1 = (1.0 / m) * np.dot(dZ1, X.T)
+    db1 = (1.0 / m) * np.sum(dZ1, axis = 1, keepdims = True)
+
+    gradients = {"dZ3": dZ3, "dW3": dW3, "db3": db3,
+                 "dA2": dA2, "dZ2": dZ2, "dW2": dW2, "db2": db2,
+                 "dA1": dA1, "dZ1": dZ1, "dW1": dW1, "db1": db1}
+
+    return gradients
+
+def gradient_check_n(parameters, gradients, X, Y, epsilon = 1e-7):
+
+    parameters_values, _ = dictionary_to_vector(parameters)
+    grad = gradients_to_vector(gradients)
+    num_parameters = parameters_values.shape[0]
+
+    J_plus = np.zeros((num_parameters, 1))
+    J_minus = np.zeros((num_parameters, 1))
+    gradapprox = np.zeros((num_parameters, 1))
+
+    for i in range(num_parameters):
+        thetaplus = np.copy(parameters_values)
+        thetaplus[i][0] = thetaplus[i] + epsilon
+        J_plus[i], _ = forward_propagation_n(X, Y,  vector_to_dictionary(thetaplus))
+
+        thetaminus = np.copy(parameters_values)
+        thetaminus[i][0] = thetaminus[i] - epsilon
+        J_minus[i], _ = forward_propagation_n(X, Y, vector_to_dictionary(thetaminus))
+
+        gradapprox[i] = (J_plus[i] - J_minus[i]) / (2 * epsilon)
+
+    difference = np.linalg.norm(grad - gradapprox) / (np.linalg.norm(grad) + np.linalg.norm(gradapprox))
+
+    if difference > 2e-7:
+        print('\033[93m' + 'Mistake in backward propagation difference. Difference is: ' + str(difference) + '\033[0m')
+    else:
+        print('\033[92m' + 'Backward propagation calculation is correct. Difference is: ' + str(difference) + '\033[0m')
+
+    return difference
