a4fca92c6a13dddb7b08a002899de5e01423a8b4
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-03 14:52:21 -0400

    included parameter updates that utilized momentum

--- Hyperparameter tuning, Regularization and Optimization/Optimization Algorithms/optimization_methods.py
+++ Hyperparameter tuning, Regularization and Optimization/Optimization Algorithms/optimization_methods.py
@@ -63,7 +63,18 @@ def initialize_velocity(parameters):
 
     return v
 
-    
+def update_parameters_with_momentum(parameters, grads, v, beta, learning_rate):
+
+    L = len(parameters) // 2
+
+    for l in range(L):
+        v['dW' + str(l + 1)] = beta * v['dW' + str(l + 1)] + (1 - beta) * grads['dW' + str(l + 1)]
+        v['db' + str(l + 1)] = beta * v['db' + str(l + 1)] + (1 - beta) * grads['db' + str(l + 1)]
+
+        parameters['W' + str(l + 1)] = parameters['W' + str(l + 1)] - learning_rate * v['dW' + str(l + 1)]
+        parameters['b' + str(l + 1)] = parameters['b' + str(l + 1)] - learning_rate * v['db' + str(l + 1)]
+
+    return parameters, v
 
 # check with GD
 parameters, grads, learning_rate = update_parameters_with_gd_test_case()
@@ -91,6 +102,19 @@ print("mini batch sanity check: " + str(mini_batches[0][0][0][0 : 3]))
 parameters = initialize_velocity_test_case()
 v = initialize_velocity(parameters)
 
+print("v[\"dW1\"] = " + str(v["dW1"]))
+print("v[\"db1\"] = " + str(v["db1"]))
+print("v[\"dW2\"] = " + str(v["dW2"]))
+print("v[\"db2\"] = " + str(v["db2"]))
+
+# check parameter updates with momentum
+parameters, grads, v = update_parameters_with_momentum_test_case()
+
+parameters, v = update_parameters_with_momentum(parameters, grads, v, beta = 0.9, learning_rate = 0.01)
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
 print("v[\"dW1\"] = " + str(v["dW1"]))
 print("v[\"db1\"] = " + str(v["db1"]))
 print("v[\"dW2\"] = " + str(v["dW2"]))
