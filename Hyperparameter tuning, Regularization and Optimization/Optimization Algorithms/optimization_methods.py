6bff13d24d9aed847c818891fa160b79013792d6
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-03 18:36:26 -0400

    added parameter update via adam optimizer

--- Hyperparameter tuning, Regularization and Optimization/Optimization Algorithms/optimization_methods.py
+++ Hyperparameter tuning, Regularization and Optimization/Optimization Algorithms/optimization_methods.py
@@ -90,6 +90,30 @@ def initialize_adam(parameters):
 
     return v, s
 
+def update_parameters_with_adam(parameters, grads, v, s, t, learning_rate = 0.01, beta1 = 0.9,  beta2 = 0.999, epsilon = 1e-8):
+
+    L = len(parameters) // 2
+    v_corrected = {}
+    s_corrected = {}
+
+    for l in range(L):
+        v['dW' + str(l + 1)] = beta1 * v['dW' + str(l + 1)] + (1 - beta1) * grads['dW' + str(l + 1)]
+        v['db' + str(l + 1)] = beta1 * v['db' + str(l + 1)] + (1 - beta1) * grads['db' + str(l + 1)]
+
+        v_corrected['dW' + str(l + 1)] = v['dW' + str(l + 1)] / (1 - np.power(beta1, t))
+        v_corrected['db' + str(l + 1)] = v['db' + str(l + 1)] / (1 - np.power(beta1, t))
+
+        s['dW' + str(l + 1)] = beta2 * s['dW' + str(l + 1)] + (1 - beta2) * np.power(grads['dW' + str(l + 1)], 2)
+        s['db' + str(l + 1)] = beta2 * s['db' + str(l + 1)] + (1 - beta2) * np.power(grads['db' + str(l + 1)], 2)
+
+        s_corrected['dW' + str(l + 1)] = s['dW' + str(l + 1)] / (1 - np.power(beta2, t))
+        s_corrected['db' + str(l + 1)] = s['db' + str(l + 1)] / (1 - np.power(beta2, t))
+
+        parameters['W' + str(l + 1)] = parameters['W' + str(l + 1)] - learning_rate * v_corrected['dW' + str(l + 1)] / np.sqrt(s_corrected['dW' + str(l + 1)] + epsilon)
+        parameters['b' + str(l + 1)] = parameters['b' + str(l + 1)] - learning_rate * v_corrected['db' + str(l + 1)] / np.sqrt(s_corrected['db' + str(l + 1)] + epsilon)
+
+        return parameters, v, s
+
 # check with GD
 parameters, grads, learning_rate = update_parameters_with_gd_test_case()
 
@@ -146,3 +170,18 @@ print("s[\"db1\"] = " + str(s["db1"]))
 print("s[\"dW2\"] = " + str(s["dW2"]))
 print("s[\"db2\"] = " + str(s["db2"]))
 
+parameters, grads, v, s = update_parameters_with_adam_test_case()
+parameters, v, s = update_parameters_with_adam(parameters, grads, v, s, t = 2)
+
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+print("v[\"dW1\"] = " + str(v["dW1"]))
+print("v[\"db1\"] = " + str(v["db1"]))
+print("v[\"dW2\"] = " + str(v["dW2"]))
+print("v[\"db2\"] = " + str(v["db2"]))
+print("s[\"dW1\"] = " + str(s["dW1"]))
+print("s[\"db1\"] = " + str(s["db1"]))
+print("s[\"dW2\"] = " + str(s["dW2"]))
+print("s[\"db2\"] = " + str(s["db2"]))
