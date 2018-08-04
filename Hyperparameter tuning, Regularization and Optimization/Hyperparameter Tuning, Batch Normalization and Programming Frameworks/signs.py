6b5a8144b3d694cf7858b5d90faecaaa3d1fa10b
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-04 15:00:58 -0400

    added flattening, grayscale to images, and one-hot encoding to labels

--- Hyperparameter tuning, Regularization and Optimization/Hyperparameter Tuning, Batch Normalization and Programming Frameworks/signs.py
+++ Hyperparameter tuning, Regularization and Optimization/Hyperparameter Tuning, Batch Normalization and Programming Frameworks/signs.py
@@ -17,3 +17,23 @@ index = 0
 plt.imshow(X_train_orig[index])
 plt.show()
 print('y = ' + str(np.squeeze(Y_train_orig[:, index])))
+
+# flatten train and test images into 1-D vectors
+X_train_flatten = X_train_orig.reshape(X_train_orig.shape[0], -1).T
+X_test_flatten = X_test_orig.reshape(X_test_orig.shape[0], -1).T
+
+# normalize image vectors to grayscale
+X_train = X_train_flatten / 255.0
+X_test = X_test_flatten / 255.0
+
+# encode labels as one-hot matrix
+Y_train = convert_to_one_hot(Y_train_orig, C = 6)
+Y_test = convert_to_one_hot(Y_test_orig, C = 6)
+
+print('number of training examples: ' + str(X_train.shape[1]))
+print('number of test examples: ' + str(X_test.shape[1]))
+print('X_train shape: ' + str(X_train.shape))
+print('Y_train shape: ' + str(Y_train.shape))
+print('X_test shape: ' + str(X_test.shape))
+print('Y_test shape: ' + str(Y_test.shape))
+
