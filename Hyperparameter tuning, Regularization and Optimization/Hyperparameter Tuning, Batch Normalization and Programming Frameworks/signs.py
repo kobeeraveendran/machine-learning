5dcd19cc0d7a6d9a107b86e7bdc31fd0d7da3870
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-05 14:23:03 -0400

    added compute cost method using tf

--- Hyperparameter tuning, Regularization and Optimization/Hyperparameter Tuning, Batch Normalization and Programming Frameworks/signs.py
+++ Hyperparameter tuning, Regularization and Optimization/Hyperparameter Tuning, Batch Normalization and Programming Frameworks/signs.py
@@ -89,3 +89,27 @@ def forward_propagation(X, parameters):
     
     return Z3
 
+tf.reset_default_graph()
+with tf.Session() as sess:
+    X, Y = create_placeholders(12288, 6)
+    parameters = initialize_parameters()
+    Z3 = forward_propagation(X, parameters)
+    print('Z3 = ' + str(Z3))
+
+def compute_cost(Z3, Y):
+    logits = tf.transpose(Z3)
+    labels = tf.transpose(Y)
+
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = logits, labels = labels))
+
+    return cost
+
+tf.reset_default_graph()
+
+with tf.Session() as sess:
+    X, Y = create_placeholders(12288, 6)
+    parameters = initialize_parameters()
+    Z3 = forward_propagation(X, parameters)
+    cost = compute_cost(Z3, Y)
+
+    print('cost = ' + str(cost))
