08fa99799d50c601586c0b2485947451c6bebf75
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-07 09:57:01 -0400

    added forward propagation for pooling layers, with check

--- Convolutional Neural Networks/Foundations of Convolutional Neural Networks/cnn-scratch.py
+++ Convolutional Neural Networks/Foundations of Convolutional Neural Networks/cnn-scratch.py
@@ -97,3 +97,53 @@ Z, cache_conv = conv_forward(A_prev, W, b, hyperparameters)
 print("Z's mean: " + str(np.mean(Z)))
 print("Z[3, 2, 1]: " + str(Z[3, 2, 1]))
 print("cache_conv[0][1][2][3]: " + str(cache_conv[0][1][2][3]))
+
+
+def pool_forward(A_prev, hyperparameters, mode = 'max'):
+
+    (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
+
+    f = hyperparameters['f']
+    stride = hyperparameters['stride']
+
+    n_H = int(1 + (n_H_prev - f) / stride)
+    n_W = int(1 + (n_W_prev - f) / stride)
+    n_C = n_C_prev
+
+    A = np.zeros(shape = (m, n_H, n_W, n_C))
+
+    for i in range(m):
+        for h in range(n_H):
+            for w in range(n_W):
+                for c in range(n_C):
+                    vert_start = h * stride
+                    vert_end = vert_start + f
+                    horiz_start = w * stride
+                    horiz_end = horiz_start + f
+
+                    a_prev_slice = A_prev[i, vert_start: vert_end, horiz_start: horiz_end, c]
+
+                    if mode == 'max':
+                        A[i, h, w, c] = np.max(a_prev_slice)
+                    elif mode == 'average':
+                        A[i, h, w, c] = np.mean(a_prev_slice)
+
+    cache = (A_prev, hyperparameters)
+
+    assert(A.shape == (m, n_H, n_W, n_C))
+
+    return A, cache
+
+# check pooling forward
+np.random.seed(1)
+A_prev = np.random.randn(2, 4, 4, 3)
+hyperparameters = {'stride': 2, 'f': 3}
+A, cache = pool_forward(A_prev, hyperparameters, mode = 'max')
+print('mode = max')
+print('A = ' + str(A))
+print('\n')
+
+A, cache = pool_forward(A_prev,  hyperparameters, mode = 'average')
+print('mode = average')
+print('A = ' + str(A))
+print('\n')
