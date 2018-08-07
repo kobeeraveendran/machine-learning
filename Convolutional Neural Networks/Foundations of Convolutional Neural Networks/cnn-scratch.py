0c1c13d5a5286b46d47ed81c13dec8efda211a91
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-07 09:12:36 -0400

    added convolutional forward propagation with checks

--- Convolutional Neural Networks/Foundations of Convolutional Neural Networks/cnn-scratch.py
+++ Convolutional Neural Networks/Foundations of Convolutional Neural Networks/cnn-scratch.py
@@ -45,3 +45,55 @@ b = np.random.randn(1, 1, 1)
 
 Z = conv_single_step(a_slice_prev, W, b)
 print('Z: ' + str(Z))
+
+def conv_forward(A_prev, W, b, hyperparameters):
+
+    (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape[0], A_prev.shape[1], A_prev.shape[2], A_prev.shape[3]
+
+    (f, f, n_C_prev, n_C) = W.shape[0], W.shape[1], W.shape[2], W.shape[3]
+
+    stride = hyperparameters['stride']
+    pad = hyperparameters['pad']
+
+    n_H = int((n_H_prev - f + 2 * pad) / stride) + 1
+    n_W = int((n_W_prev - f + 2 * pad) / stride) + 1
+
+    Z = np.zeros(shape = (m, n_H, n_W, n_C))
+
+    A_prev_pad = zero_pad(A_prev, pad)
+
+    for i in range(m):
+        a_prev_pad = A_prev_pad[i]
+
+        for h in range(n_H):
+            for w in range(n_W):
+                for c in range(n_C):
+
+                    vert_start = h * stride
+                    vert_end = vert_start + f
+                    horiz_start = w * stride
+                    horiz_end = horiz_start + f
+
+                    a_slice_prev = a_prev_pad[vert_start: vert_end, horiz_start: horiz_end, :]
+
+                    Z[i, h, w, c] = conv_single_step(a_slice_prev, W[:, :, :, c], b[:, :, :, c])
+
+    assert(Z.shape == (m, n_H, n_W, n_C))
+
+    cache = (A_prev, W, b, hyperparameters)
+
+    return Z, cache
+
+# check implementation of conv_forward
+np.random.seed(1)
+A_prev = np.random.randn(10, 4, 4, 3)
+W = np.random.randn(2, 2, 3, 8)
+b = np.random.randn(1, 1, 1, 8)
+
+hyperparameters = {'pad': 2, 'stride': 2}
+
+Z, cache_conv = conv_forward(A_prev, W, b, hyperparameters)
+
+print("Z's mean: " + str(np.mean(Z)))
+print("Z[3, 2, 1]: " + str(Z[3, 2, 1]))
+print("cache_conv[0][1][2][3]: " + str(cache_conv[0][1][2][3]))
