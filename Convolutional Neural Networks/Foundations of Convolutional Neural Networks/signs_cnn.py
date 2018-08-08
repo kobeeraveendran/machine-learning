fe671bb92a5a80dba00f01643d3f144878a98441
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-08 15:21:51 -0400

    added model with forward/backprop and optimizers

--- Convolutional Neural Networks/Foundations of Convolutional Neural Networks/signs_cnn.py
+++ Convolutional Neural Networks/Foundations of Convolutional Neural Networks/signs_cnn.py
@@ -114,3 +114,67 @@ with tf.Session() as sess:
 
     print('cost = ' + str(a))
 
+def model(X_train, Y_train, X_test, Y_test, learning_rate = 0.009, num_epochs = 100, minibatch_size = 64, print_cost = True):
+
+    ops.reset_default_graph
+
+    tf.set_random_seed(1)
+    seed = 3
+    m, n_H0, n_W0, n_C0 = X_train.shape
+    n_y = Y_train.shape[1]
+    costs = []
+
+    X, Y = create_placeholders(n_H0, n_W0, n_C0, n_y)
+
+    parameters = initialize_parameters()
+
+    Z3 = forward_propagation(X, parameters)
+
+    cost = compute_cost(Z3, Y)
+
+    optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate).minimize(cost)
+
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as sess:
+        sess.run(init)
+
+        for epoch in range(num_epochs):
+            minibatch_cost = 0.0
+            num_minibatches = int(m / minibatch_size)
+            seed += 1
+            minibatches = random_mini_batches(X_train, Y_train, minibatch_size, seed)
+
+            for minibatch in minibatches:
+                minibatch_X, minibatch_Y = minibatch
+
+                _, temp_cost = sess.run([optimizer, cost], feed_dict = {X: minibatch_X, Y: minibatch_Y})
+
+                minibatch_cost += temp_cost / num_minibatches
+
+            if print_cost and epoch % 5 == 0:
+                print('Cost after epoch {}: {}'.format(epoch, minibatch_cost))
+
+            if print_cost:
+                costs.append(minibatch_cost)
+
+        plt.plot(np.squeeze(costs))
+        plt.ylabel('cost')
+        plt.xlabel('iterations (tens)')
+        plt.title('Learning rate = ' + str(learning_rate))
+        plt.show()
+
+        predict_op = tf.argmax(Z3, 1)
+        correct_prediction = tf.equal(predict_op, tf.argmax(Y, 1))
+
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
+        print(accuracy)
+
+        train_accuracy = accuracy.eval({X: X_train, Y: Y_train})
+        test_accuracy = accuracy.eval({X: X_test, Y: Y_test})
+        print('Training accuracy: ' + str(train_accuracy))
+        print('Testing accuracy: ' + str(test_accuracy))
+
+    return train_accuracy, test_accuracy, parameters
+
+    
