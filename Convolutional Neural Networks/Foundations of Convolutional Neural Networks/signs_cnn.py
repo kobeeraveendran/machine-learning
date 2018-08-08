edf9ebb1d588623c2f6ef2078ef4bc9098c9bad5
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-08 14:51:55 -0400

    added forward prop: 2 x (conv2d -> relu -> max_pool) -> flatten -> dense

--- Convolutional Neural Networks/Foundations of Convolutional Neural Networks/signs_cnn.py
+++ Convolutional Neural Networks/Foundations of Convolutional Neural Networks/signs_cnn.py
@@ -65,4 +65,31 @@ with tf.Session() as sess:
 
     print('W1 = ' + str(parameters['W1'].eval()[1, 1, 1]))
     print('W2 = ' + str(parameters['W2'].eval()[1, 1, 1]))
-    
+
+def forward_propagation(X, parameters):
+    W1 = parameters['W1']
+    W2 = parameters['W2']
+
+    Z1 = tf.nn.conv2d(X, filter = W1, strides = [1, 1, 1, 1], padding = 'SAME')
+    A1 = tf.nn.relu(Z1)
+    P1 = tf.nn.max_pool(A1, ksize = [1, 8, 8, 1], strides = [1, 8, 8, 1], padding = 'SAME')
+    Z2 = tf.nn.conv2d(P1, filter = W2, strides = [1, 1, 1, 1], padding = 'SAME')
+    A2 = tf.nn.relu(Z2)
+    P2 = tf.nn.max_pool(A2, ksize = [1, 4, 4, 1], strides = [1, 4, 4, 1], padding = 'SAME')
+    P2 = tf.contrib.layers.flatten(P2)
+    Z3 = tf.contrib.layers.fully_connected(P2, 6, activation_fn = None)
+
+    return Z3
+
+tf.reset_default_graph()
+
+with tf.Session() as sess:
+    np.random.seed(1)
+    X, Y = create_placeholders(64, 64, 3, 6)
+    parameters = initialize_parameters()
+    Z3 = forward_propagation(X, parameters)
+    init = tf.global_variables_initializer()
+    sess.run(init)
+    a = sess.run(Z3, {X: np.random.randn(2, 64, 64, 3), Y: np.random.randn(2, 6)})
+    print('Z3 = ' + str(Z3))
+
