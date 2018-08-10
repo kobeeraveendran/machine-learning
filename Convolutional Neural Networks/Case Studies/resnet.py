672f1f75d180e2dd501c8a3d699f1afc11aaa850
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-10 14:11:24 -0400

    added model and fixed tf/keras issue using 'Add' layer instead of add function

--- Convolutional Neural Networks/Case Studies/resnet.py
+++ Convolutional Neural Networks/Case Studies/resnet.py
@@ -44,7 +44,7 @@ def identity_block(X, f, filters, stage, block):
     X = Conv2D(filters = F3, kernel_size = (1, 1), strides = (1, 1), padding = 'valid', name = conv_name_base + '2c', kernel_initializer = glorot_uniform(seed = 0))(X)
     X = BatchNormalization(axis = 3, name = bn_name_base + '2c')(X)
 
-    X = X + X_shortcut
+    X = Add()([X, X_shortcut])
     X = Activation('relu')(X)
 
     return X
@@ -83,7 +83,7 @@ def convolutional_block(X, f, filters, stage, block, s = 2):
     X_shortcut = Conv2D(filters = F3, kernel_size = (1, 1), strides = (s, s), name = conv_name_base + '1', kernel_initializer = glorot_uniform(seed = 0))(X_shortcut)
     X_shortcut = BatchNormalization(axis = 3, name = bn_name_base + '1')(X_shortcut)
 
-    X = X + X_shortcut
+    X = Add()([X, X_shortcut])
     X = Activation('relu')(X)
 
     return X
@@ -98,3 +98,52 @@ with tf.Session() as test:
     test.run(tf.global_variables_initializer())
     out = test.run([A], feed_dict={A_prev: X, K.learning_phase(): 0})
     print("out = " + str(out[0][1][1][0]))
+
+
+def ResNet50(input_shape = (64, 64, 3), classes = 6):
+
+    X_input = Input(input_shape)
+
+    X = ZeroPadding2D((3, 3))(X_input)
+
+    X = Conv2D(64, (7, 7), strides = (2, 2), name = 'conv1', kernel_initializer = glorot_uniform(seed = 0))(X)
+    X = BatchNormalization(axis = 3, name = 'bn_conv1')(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((3, 3), strides = (2, 2))(X)
+
+    X = convolutional_block(X, f = 3, filters = [64, 64, 256], stage = 2, block = 'a', s = 1)
+    X = identity_block(X, f = 3, filters = [64, 64, 256], stage = 2, block = 'b')
+    X = identity_block(X, f = 3, filters = [64, 64, 256], stage = 2, block = 'c')
+
+    # stage 3
+    X = convolutional_block(X, f = 3, filters = [128, 128, 512], stage = 3, block = 'a', s = 2)
+    X = identity_block(X, f = 3, filters = [128, 128, 512], stage = 3, block = 'b')
+    X = identity_block(X, f = 3, filters = [128, 128, 512], stage = 3, block = 'c')
+    X = identity_block(X, f = 3, filters = [128, 128, 512], stage = 3, block = 'd')
+
+    # stage 4
+    X = convolutional_block(X, f = 3, filters = [256, 256, 1024], stage = 4, block = 'a', s = 2)
+    X = identity_block(X, f = 3, filters = [256, 256, 1024], stage = 4, block = 'b')
+    X = identity_block(X, f = 3, filters = [256, 256, 1024], stage = 4, block = 'c')
+    X = identity_block(X, f = 3, filters = [256, 256, 1024], stage = 4, block = 'd')
+    X = identity_block(X, f = 3, filters = [256, 256, 1024], stage = 4, block = 'e')
+    X = identity_block(X, f = 3, filters = [256, 256, 1024], stage = 4, block = 'f')
+
+    # stage 5
+    X = convolutional_block(X, f = 3, filters = [512, 512, 2048], stage = 5, block = 'a', s = 2)
+    X = identity_block(X, f = 3, filters = [512, 512, 2048], stage = 5, block = 'b')
+    X = identity_block(X, f = 3, filters = [512, 512, 2048], stage = 5, block = 'c')
+
+    # average pooling
+    X = AveragePooling2D(pool_size = (2, 2), name = 'avg_pool', padding = 'same')(X)
+    
+    X = Flatten()(X)
+    X = Dense(classes, activation = 'softmax', name = 'fc' + str(classes), kernel_initializer = glorot_uniform(seed = 0))(X)
+
+    model = Model(inputs = X_input, outputs = X, name = 'ResNet50')
+
+    return model
+
+model = ResNet50(input_shape = (64, 64, 3), classes = 6)
+
+model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
