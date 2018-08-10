39aef443d98d36c7a3222da967f19d722a9f7231
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-10 12:55:04 -0400

    merged with my code from notebook for uniformity

--- Convolutional Neural Networks/Case Studies/resnet.py
+++ Convolutional Neural Networks/Case Studies/resnet.py
@@ -33,17 +33,14 @@ def identity_block(X, f, filters, stage, block):
 
     X_shortcut = X
 
-    # first component
-    X = Conv2D(filters = F1, kernel_size = (1, 1), strides = (1, 1), padding = 'valid', name = conv_name_base + '2a', kernel_initializer = glorot_uniform(seed = 0))(X)
+    X = Conv2D(filters = F1, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2a', kernel_initializer = glorot_uniform(seed=0))(X)
     X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
     X = Activation('relu')(X)
 
-    # second component
     X = Conv2D(filters = F2, kernel_size = (f, f), strides = (1, 1), padding = 'same', name = conv_name_base + '2b', kernel_initializer = glorot_uniform(seed = 0))(X)
     X = BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
     X = Activation('relu')(X)
 
-    # third component
     X = Conv2D(filters = F3, kernel_size = (1, 1), strides = (1, 1), padding = 'valid', name = conv_name_base + '2c', kernel_initializer = glorot_uniform(seed = 0))(X)
     X = BatchNormalization(axis = 3, name = bn_name_base + '2c')(X)
 
@@ -56,12 +53,12 @@ tf.reset_default_graph()
 
 with tf.Session() as test:
     np.random.seed(1)
-    A_prev = tf.placeholder('float', [3, 4, 4, 6])
+    A_prev = tf.placeholder("float", [3, 4, 4, 6])
     X = np.random.randn(3, 4, 4, 6)
     A = identity_block(A_prev, f = 2, filters = [2, 4, 6], stage = 1, block = 'a')
     test.run(tf.global_variables_initializer())
-    out = test.run([A], feed_dict = {A_prev: X, K.learning_phase(): 0})
-    print('out = ' + str(out[0][1][1][0]))
+    out = test.run([A], feed_dict={A_prev: X, K.learning_phase(): 0})
+    print("out = " + str(out[0][1][1][0]))
 
 def convolutional_block(X, f, filters, stage, block, s = 2):
 
