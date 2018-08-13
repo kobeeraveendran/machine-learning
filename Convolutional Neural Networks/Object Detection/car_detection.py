5a4463b6f290d3c160f92834ca9c09193edbc3cf
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-13 19:22:49 -0400

    added non-max suppression function

--- Convolutional Neural Networks/Object Detection/car_detection.py
+++ Convolutional Neural Networks/Object Detection/car_detection.py
@@ -68,3 +68,16 @@ def iou(box1, box2):
     iou = inter_area / union_area
 
     return iou
+
+def yolo_non_max_suppression(scores, boxes, classes, max_boxes = 10, iou_threshold = 0.5):
+
+    max_boxes_tensor = K.variable(max_boxes, dtype = 'int32')
+    K.get_session().run(tf.variables_initializer([max_boxes_tensor]))
+
+    nms_indices = tf.image.non_max_suppression(boxes, scores, max_output_size = max_boxes_tensor, iou_threshold = iou_threshold)
+
+    scores = K.gather(scores, nms_indices)
+    boxes = K.gather(boxes, nms_indices)
+    classes = K.gather(classes, nms_indices)
+
+    return scores, boxes, classes
