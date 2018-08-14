22bca7f3bf221614dab8ed47c2f34eae003f266f
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-14 16:46:11 -0400

    loaded pre-trained VGG model, added compute content cost function, and added style image (Monet)

--- Convolutional Neural Networks/Special Applications/neural_style_transfer.py
+++ Convolutional Neural Networks/Special Applications/neural_style_transfer.py
@@ -11,3 +11,30 @@ import tensorflow as tf
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '4'
 
+model = load_vgg_model('pretrained-model/imagenet-vgg-verydeep-19.mat')
+
+# content image
+content_image = scipy.misc.imread('images/louvre.jpg')
+imshow(content_image)
+
+
+def compute_content_cost(a_C, a_G):
+    m, n_H, n_W, n_C = a_G.get_shape().as_list()
+
+    a_C_unrolled = tf.reshape(a_C, shape = (n_H * n_W, n_C))
+    a_G_unrolled = tf.reshape(a_G, shape = (n_H * n_W, n_C))
+
+    J_content = (1 / (4 * n_H * n_W * n_C)) * tf.reduce_sum(tf.square(tf.subtract(a_C_unrolled, a_G_unrolled)))
+
+    return J_content
+
+tf.reset_default_graph()
+
+with tf.Session() as test:
+    tf.set_random_seed(1)
+    a_C = tf.random_normal([1, 4, 4, 3], mean = 1, stddev = 4)
+    a_G = tf.random_normal([1, 4, 4, 3], mean = 1, stddev = 4)
+
+    J_content = compute_content_cost(a_C, a_G)
+    print('J_content = ', J_content.eval())
+
