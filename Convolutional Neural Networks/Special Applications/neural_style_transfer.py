908c1ad902f05a86538d8cedd4e82b808bb02194
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-14 16:22:08 -0400

    updated gitignore to ignore pretrained model, added imports

---
+++ Convolutional Neural Networks/Special Applications/neural_style_transfer.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import scipy.io
+import scipy.misc
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+from PIL import Image
+from nst_utils import *
+import numpy as np
+import tensorflow as tf
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '4'
+
