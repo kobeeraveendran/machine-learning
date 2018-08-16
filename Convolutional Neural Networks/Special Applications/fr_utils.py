30a0d0ab250a78c41f224cfbfb5382ec8472a10a
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-15 22:04:34 -0400

    fixed small bug in fr_utils, added inception_blocks_v2 from hub

--- Convolutional Neural Networks/Special Applications/fr_utils.py
+++ Convolutional Neural Networks/Special Applications/fr_utils.py
@@ -38,7 +38,7 @@ def LRN2D(x):
     return tf.nn.lrn(x, alpha = 1e-4, beta = 0.75)
 
 def conv2d_bn(x, layer = None, cv1_out = None, cv1_filter = (1, 1), cv1_strides = (1, 1), cv2_out = None, cv2_filter = (3, 3), cv2_strides = (1, 1), padding = None):
-    num = '' if cv2_out = None else '1'
+    num = '' if cv2_out == None else '1'
     
     tensor = Conv2D(cv1_out, cv1_filter, strides = cv1_strides, data_format = 'channels_first', name = layer + '_conv' + num)(x)
     tensor = BatchNormalization(axis=1, epsilon=0.00001, name=layer+'_bn'+num)(tensor)
