aee9acf3187270c9f23961b5c669dab3e0f01c59
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-16 17:32:16 -0400

    added database, database images, compiled model, and loaded pre-trained model weights

--- Convolutional Neural Networks/Special Applications/fr_happyhouse.py
+++ Convolutional Neural Networks/Special Applications/fr_happyhouse.py
@@ -53,3 +53,22 @@ with tf.Session() as test:
     loss = triplet_loss(y_true, y_pred)
     print('loss = ', loss.eval())
 
+# compile model and load pre-trained weights
+FRmodel.compile(optimizer = 'adam', loss = triplet_loss, metrics = ['accuracy'])
+load_weights_from_FaceNet(FRmodel)
+
+database = {}
+
+database['danielle'] = img_to_encoding('images/danielle.png', FRmodel)
+database['younes'] = img_to_encoding('images/younes.jpg', FRmodel)
+database['tian'] = img_to_encoding('images/tian.jpg', FRmodel)
+database['andrew'] = img_to_encoding('images/andrew.jpg', FRmodel)
+database['kian'] = img_to_encoding('images/kian.jpg', FRmodel)
+database['dan'] = img_to_encoding('images/dan.jpg', FRmodel)
+database['sebastiano'] = img_to_encoding('images/sebastiano.jpg', FRmodel)
+database['bertrand'] = img_to_encoding('images/bertrand.jpg', FRmodel)
+database['kevin'] = img_to_encoding('images/kevin.jpg', FRmodel)
+database['felix'] = img_to_encoding('images/felix.jpg', FRmodel)
+database['benoit'] = img_to_encoding('images/benoit.jpg', FRmodel)
+database['arnaud'] = img_to_encoding('images/arnaud.jpg', FRmodel)
+
