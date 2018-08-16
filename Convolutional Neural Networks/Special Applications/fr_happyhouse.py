56d45bac65d6591271ef45427acdce86b3004b04
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-16 17:07:16 -0400

    added test case for triplet loss

--- Convolutional Neural Networks/Special Applications/fr_happyhouse.py
+++ Convolutional Neural Networks/Special Applications/fr_happyhouse.py
@@ -42,3 +42,14 @@ def triplet_loss(y_true, y_pred, alpha = 0.2):
 
     return loss
 
+# check triplet loss
+with tf.Session() as test:
+    tf.set_random_seed(1)
+    y_true = (None, None, None)
+    y_pred = (tf.random_normal([3, 128], mean = 6, stddev = 0.1, seed = 1), 
+              tf.random_normal([3, 128], mean = 1, stddev = 1, seed = 1), 
+              tf.random_normal([3, 128], mean = 3, stddev = 4, seed = 1))
+
+    loss = triplet_loss(y_true, y_pred)
+    print('loss = ', loss.eval())
+
