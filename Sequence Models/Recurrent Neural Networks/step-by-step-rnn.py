8bc3b8431555f2076fdda3aa1c38f25e9c733190
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-17 17:14:10 -0400

    fixed rnn forward prop dimension issue

--- Sequence Models/Recurrent Neural Networks/step-by-step-rnn.py
+++ Sequence Models/Recurrent Neural Networks/step-by-step-rnn.py
@@ -35,3 +35,29 @@ print('a_next[4] = ', a_next[4])
 print('a_next shape: ', a_next.shape)
 print('yt_pred[1] = ', yt_pred[1])
 print('yt_pred shape: ', yt_pred.shape)
+
+
+# forward prop (cycle the single-step RNN cells over T time-steps)
+def rnn_forward(x, a0, parameters):
+
+    caches = []
+
+    n_x, m, T_x = x.shape
+    n_y, n_a = parameters['Wya'].shape
+
+    a = np.zeros((n_a, m, T_x))
+    y_pred = np.zeros((n_y, m, T_x))
+
+    a_next = a0
+
+    for t in range(T_x):
+
+        a_next, yt_pred, cache = rnn_cell_forward(x[..., t], a_next, parameters)
+        a[..., t] = a_next
+        y_pred[..., t] = yt_pred
+
+        caches.append(cache)
+
+    caches = (caches, x)
+
+    return a, yt_pred, caches
