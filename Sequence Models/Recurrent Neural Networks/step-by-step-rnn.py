6429e233940f943e422d89e837e1c3ff9bd59f03
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-17 16:46:07 -0400

    added rnn cell operations for a single time-step

--- Sequence Models/Recurrent Neural Networks/step-by-step-rnn.py
+++ Sequence Models/Recurrent Neural Networks/step-by-step-rnn.py
@@ -1,3 +1,20 @@
 import numpy as np
 from rnn_utils import softmax, sigmoid, initialize_adam, update_parameters_with_adam
 
+# RNN operations for a single time-step
+def rnn_cell_forward(xt, a_prev, parameters):
+
+    Wax = parameters['Wax']
+    Waa = parameters['Waa']
+    Wya = parameters['Wya']
+    ba = parameters['ba']
+    by = parameters['by']
+
+    a_next = np.tanh(np.dot(Waa, a_prev) + np.dot(Wax, xt) + ba)
+
+    yt_pred = softmax(np.dot(Wya, a_next) + by)
+
+    cache = (a_next, a_prev, xt, parameters)
+
+    return a_next, yt_pred, cache
+
