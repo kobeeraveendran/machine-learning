c9691f8d965bc3e8305f6cba3b892058dfee2329
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-17 18:56:14 -0400

    added test case with auto check for single-step forwardprop LSTM cell

--- Sequence Models/Recurrent Neural Networks/step-by-step-rnn.py
+++ Sequence Models/Recurrent Neural Networks/step-by-step-rnn.py
@@ -132,3 +132,42 @@ def lstm_cell_forward(xt, a_prev, c_prev, parameters):
     cache = (a_next, c_next, a_prev, c_prev, ft, it, cct, ot, xt, parameters)
 
     return a_next, c_next, yt_pred, cache
+
+np.random.seed(1)
+xt = np.random.randn(3, 10)
+a_prev = np.random.randn(5, 10)
+c_prev = np.random.randn(5, 10)
+Wf = np.random.randn(5, 5 + 3)
+bf = np.random.randn(5, 1)
+Wi = np.random.randn(5, 5 + 3)
+bi = np.random.randn(5, 1)
+Wo = np.random.randn(5, 5 + 3)
+bo = np.random.randn(5, 1)
+Wc = np.random.randn(5, 5 + 3)
+bc = np.random.randn(5, 1)
+Wy = np.random.randn(2, 5)
+by = np.random.randn(2, 1)
+
+parameters = {"Wf": Wf, "Wi": Wi, "Wo": Wo, "Wc": Wc, "Wy": Wy, "bf": bf, "bi": bi, "bo": bo, "bc": bc, "by": by}
+
+a_next, c_next, yt, cache = lstm_cell_forward(xt, a_prev, c_prev, parameters)
+
+print('\n\nLSTM single cell forward prop check:')
+
+np.testing.assert_array_almost_equal(a_next[4], [-0.66408471, 0.0036921, 0.02088357, 0.22834167, -0.85575339, 0.00138482, 0.76566531, 0.34631421, -0.00215674, 0.43827275], err_msg = 'Failed.')
+print('Passed.')
+
+print('Passed.') if a_next.shape == (5, 10) else print('Failed.')
+
+np.testing.assert_array_almost_equal(c_next[2], [0.63267805, 1.00570849, 0.35504474, 0.20690913, -1.64566718, 0.11832942, 0.76449811, -0.0981561, -0.74348425, -0.26810932], err_msg = 'Failed.')
+print('Passed.')
+
+print('Passed.') if c_next.shape == (5, 10) else print('Failed.')
+
+np.testing.assert_array_almost_equal(yt[1], [0.79913913, 0.15986619, 0.22412122, 0.15606108, 0.97057211, 0.31146381, 0.00943007, 0.12666353, 0.39380172, 0.07828381], err_msg = 'Failed.')
+print('Passed.')
+
+np.testing.assert_array_almost_equal(cache[1][3], [-0.16263996, 1.03729328, 0.72938082, -0.54101719, 0.02752074, -0.30821874, 0.07651101, -1.03752894, 1.41219977, -0.37647422], err_msg = 'Failed.')
+print('Passed.')
+
+print('Passed.') if len(cache) == 10 else print('Failed.')
