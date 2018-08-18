453520d2b23cfdf217b0b5f8694efd97c01f8d2c
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-18 15:34:59 -0400

    added backward propagation through all T_x time steps for RNN

--- Sequence Models/Recurrent Neural Networks/step-by-step-rnn.py
+++ Sequence Models/Recurrent Neural Networks/step-by-step-rnn.py
@@ -320,3 +320,37 @@ np.testing.assert_array_almost_equal(gradients['dba'][4], [0.80517166], err_msg 
 print('Passed.')
 
 print('Passed.') if gradients['dba'].shape == (5, 1) else print('Failed.')
+
+
+# RNN backprop through all time steps
+def rnn_backward(da, caches):
+
+    (caches, x) = caches
+    (a1, a0, x1, parameters) = caches[0]
+
+    n_a, m, T_x = da.shape
+    n_x, m = x1.shape
+
+    dx = np.zeros((n_x, m, T_x))
+    dWax = np.zeros((n_a, n_x))
+    dWaa = np.zeros((n_a, n_a))
+    dba = np.zeros((n_a, 1))
+    da0 = np.zeros((n_a, m))
+    da_prevt = np.zeros((n_a, m))
+
+    for t in reversed(range(T_x)):
+
+        gradients = rnn_cell_backward(da[..., t] + da_prevt, caches[t])
+        dxt, da_prevt, dWaxt, dWaat, dbat = gradients['dxt'], gradients['da_prev'], gradients['dWax'], gradients['dWaa'], gradients['dba']
+
+        dx[..., t] = dxt
+        dWax += dWaxt
+        dWaa += dWaat
+        dba += dbat
+
+    da0 = da_prevt
+
+    gradients = {'dx': dx, 'da0': da0, 'dWax': dWax, 'dWaa': dWaa, 'dba': dba}
+
+    return gradients
+
