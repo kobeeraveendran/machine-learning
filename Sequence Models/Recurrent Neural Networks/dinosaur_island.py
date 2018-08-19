48a81a999d22640f2d7fb0acba805a30d12e1e74
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-19 16:45:42 -0400

    added gradient clipping, included corpus of dinosaur names

--- Sequence Models/Recurrent Neural Networks/dinosaur_island.py
+++ Sequence Models/Recurrent Neural Networks/dinosaur_island.py
@@ -1,3 +1,28 @@
 import numpy as np
 from utils import *
 import random
+
+# preprocessing
+data = open('dinos.txt', 'r').read()
+data = data.lower()
+chars = list(set(data))
+data_size, vocab_size = len(data), len(chars)
+print('There are {} total characters and {} total unique characters in the dataset.'.format(data_size, vocab_size))
+
+char_to_ix = { ch:i for i, ch in enumerate(sorted(chars)) }
+ix_to_char = { i:ch for i, ch in enumerate(sorted(chars)) }
+
+print(ix_to_char)
+
+def clip(gradients, maxValue):
+
+    dWaa, dWax, dWya, db, dby = gradients['dWaa'], gradients['dWax'], gradients['dWya'], gradients['db'], gradients['dby']
+
+    for gradient in [dWax, dWaa, dWya, db, dby]:
+        np.clip(gradient, a_min = -maxValue, a_max = maxValue, out = gradient)
+
+    gradients = {'dWaa': dWaa, 'dWax': dWax, 'dWya': dWya, 'db': db, 'dby': dby}
+
+    return gradients
+
+    
