3040c11ddb2564fa26f7a7d8e7d103c51a126b60
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-20 22:20:53 -0400

    added sample jazz sounds (initial and generated)

--- Sequence Models/Recurrent Neural Networks/jazz_solo/jazz_improv.py
+++ Sequence Models/Recurrent Neural Networks/jazz_solo/jazz_improv.py
@@ -4,6 +4,11 @@ import sys
 
 import numpy as np
 from music21 import *
+from grammar import *
+from qa import *
+from preprocess import *
+from music_utils import *
+from data_utils import *
 
 from keras.models import load_model, Model
 from keras.layers import Dense, Activation, Dropout, Input, LSTM, Reshape, Lambda, RepeatVector
@@ -11,3 +16,5 @@ from keras.initializers import glorot_uniform
 from keras.utils import to_categorical
 from keras.optimizers import Adam
 from keras import backend as K
+
+IPython.display.Audio('./data/30s_seq.mp3')
