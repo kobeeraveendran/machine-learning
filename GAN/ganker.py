d20109430f2d03a63c30719065f18ea1b3a719e4
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2018-08-24 22:49:12 -0400

    added generator model architecture w/ summary

--- GAN/ganker.py
+++ GAN/ganker.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 
 import keras
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, Conv2D, LeakyReLU, Dropout, Activation
+from keras.layers import Dense, Flatten, Conv2D, LeakyReLU, Dropout, Activation, BatchNormalization, Reshape
+from keras.layers import UpSampling2D, Conv2DTranspose
 from keras.regularizers import L1L2
 
 from keras_adversarial import AdversarialModel, simple_gan, gan_targets
@@ -16,6 +17,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
+# discriminator model architecture
 d_model = Sequential()
 dropout = 0.4
 
@@ -38,3 +40,36 @@ d_model.add(Dense(units = 1))
 d_model.add(Activation(activation = 'sigmoid'))
 
 d_model.summary()
+
+
+# generator model architecture
+g_model = Sequential()
+
+# in: 100
+# out: 7 x 7 x 256
+g_model.add(Dense(units = 7 * 7 * 256, input_dim = 100))
+g_model.add(BatchNormalization(momentum = 0.9))
+g_model.add(Activation(activation = 'relu'))
+g_model.add(Reshape((7, 7, 256)))
+g_model.add(Dropout(dropout))
+
+# in: 7 x 7 x 256
+# out: 14 x 14 x 128 -> 28 x 28 x 64 -> 28 x 28 x 32
+g_model.add(UpSampling2D())
+g_model.add(Conv2DTranspose(128, kernel_size = 5, padding = 'same'))
+g_model.add(BatchNormalization(momentum = 0.9))
+g_model.add(Activation('relu'))
+g_model.add(UpSampling2D())
+g_model.add(Conv2DTranspose(64, kernel_size = 5, padding = 'same'))
+g_model.add(BatchNormalization(momentum = 0.9))
+g_model.add(Activation(activation = 'relu'))
+g_model.add(Conv2DTranspose(32, kernel_size = 5, padding = 'same'))
+g_model.add(BatchNormalization(momentum = 0.9))
+g_model.add(Activation(activation = 'relu'))
+
+# in: 28 x 28 x 32
+# out: 28 x 28 x 1
+g_model.add(Conv2DTranspose(1, kernel_size = 5, padding = 'same'))
+g_model.add(Activation(activation = 'sigmoid'))
+
+g_model.summary()
