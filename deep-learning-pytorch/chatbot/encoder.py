be3ce83789d92b95edd151eee4d5cd662ee30e89
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2019-02-15 15:16:20 -0500

    added encoder class

---
+++ deep-learning-pytorch/chatbot/encoder.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+
+class EncoderRNN(nn.module):
+    
+    def __init__(self, hidden_size, embedding, n_layers = 1, dropout = 0):
+        super(EncoderRNN, self).__init__()
+
+        self.n_layers = n_layers
+        self.hidden_size = hidden_size
+        self.embedding = embedding
+
+        # gru init
+        self.gru = nn.GRU(hidden_size, hidden_size, n_layers, 
+                          dropout = (0 if n_layers == 1 else dropout), bidirectional = True)
+
+    def forward(self, input_seq, input_lengths, hidden = None):
+        embedded = self.embedding(input_seq)
+        packed = torch.nn.utils.rnn.pack_padded_sequence(embedded, input_lengths)
+        outputs, hidden = self.gru(packed, hidden)
+        outputs, _ = torch.nn.utils.rnn.pad_packed_sequence(outputs)
+        outputs = outputs[:, :, :self.hidden_size] + outputs[:, :, self.hidden_size:]
+
+        return outputs, hidden
