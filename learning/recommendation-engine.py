2c298e1cdc50e2b0d307c2de0ca00f0c52adebe5
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2019-02-18 16:50:45 -0500

    added product recommendation example with included product dataset

---
+++ learning/recommendation-engine.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+
+ds = pd.read_csv('data/sample-data.csv')
+
+tf = TfidfVectorizer(analyzer = 'word', ngram_range = (1, 3), min_df = 0, stop_words = 'english')
+tfidf_matrix = tf.fit_transform(ds['description'])
+
+cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+results = {}
+
+for idx, row in ds.iterrows():
+    similar_indices = cosine_similarities[idx].argsort()[:-100:-1]
+    similar_items = [(cosine_similarities[idx][i], ds['id'][i]) for i in similar_indices]
+
+    results[row['id']] = similar_items[1:]
+
+print('Complete.')
+
+# prediction
+def item(id):
+    return ds.loc[ds['id'] == id]['description'].tolist()[0].split(' - ')[0]
+
+def recommend(item_id, num):
+    print('Recommending {0} products similar to {1} ...'.format(num, item(item_id)))
+    print('-------')
+
+    recs = results[item_id][:num]
+
+    for rec in recs:
+        print('Recommended: {0} (score: {1})' .format(item(rec[1]), rec[0]))
+
+
+recommend(item_id = 11, num = 5)
